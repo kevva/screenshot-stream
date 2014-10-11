e70229828d95a331f7868ef9befe4bd0e9044c1a
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-11 14:55:15 +0200

    Set `err.noStack`

--- index.js
+++ index.js
@@ -18,10 +18,13 @@ var spawn = require('child_process').spawn;
 
 module.exports = function (url, size, opts) {
 	if (!phantomjs) {
-		throw new Error([
+		var err = new Error([
 			'The automatic install of PhantomJS, which is used for generating the screenshots, seems to have failed.',
 			'Try installing it manually: http://phantomjs.org/download.html'
 		].join('\n'));
+
+		err.noStack = true;
+		throw err;
 	}
 
 	opts = opts || {};
