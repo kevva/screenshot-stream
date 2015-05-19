1bde56fc86a94c53eab2cd9b36b59d86d8b9fc81
ecorkevin <kevinecor@gmail.com>
2015-05-19 14:05:32 -0500

    Make `es5-shim` optional
    Fixes #16.

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ var base64Stream = require('base64-stream');
 var es5Shim = require.resolve('es5-shim');
 var parseCookiePhantomjs = require('parse-cookie-phantomjs');
 var phantomBridge = require('phantom-bridge');
+var es5shim;
 
 module.exports = function (url, size, opts) {
 	opts = opts || {};
 	opts.url = url;
-	opts.es5shim = path.relative(path.join(__dirname, 'lib'), es5Shim);
 	opts.delay = opts.delay || 0;
 	opts.scale = opts.scale || 1;
 	opts.width = size.split(/x/i)[0] * opts.scale;
 	opts.height = size.split(/x/i)[1] * opts.scale;
+	opts.es5shim = opts.es5shim !== false ? path.relative(path.join(__dirname, 'lib'), es5Shim) : null;
 	opts.format = opts.format === 'jpg' ? 'jpeg' : opts.format ? opts.format : 'png';
 	opts.cookies = (opts.cookies || []).map(function (cookie) {
 		return typeof cookie === 'string' ? parseCookiePhantomjs(cookie) : cookie;
 	});
+
+	if (opts.es5Shim) {
+		es5shim = fs.readFileSync(es5Shim, 'utf8');
+	}
 
 	var cp = phantomBridge(path.join(__dirname, 'lib/index.js'), [
 		'--ignore-ssl-errors=true',
@@ -28,7 +33,6 @@ module.exports = function (url, size, opts) {
 	]);
 
 	var stream = cp.stdout.pipe(base64Stream.decode());
-	var es5shim = fs.readFileSync(es5Shim, 'utf8');
 
 	process.stderr.setMaxListeners(0);
 
@@ -44,7 +48,7 @@ module.exports = function (url, size, opts) {
 			return;
 		}
 
-		if (es5shim.indexOf(data) !== -1) {
+		if (es5shim && es5shim.indexOf(data) !== -1) {
 			return;
 		}
 
