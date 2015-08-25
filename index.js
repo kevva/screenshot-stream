4af26ed2171a698ae4ea21cf649a4db65c53580a
Sindre Sorhus <sindresorhus@gmail.com>
2015-08-26 04:56:34 +0700

    handle cookies without `domain` and `path`
    fixes https://github.com/sindresorhus/pageres/issues/200

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 'use strict';
 var fs = require('fs');
 var path = require('path');
+var urlMod = require('url');
 var base64Stream = require('base64-stream');
 var es5Shim = require.resolve('es5-shim');
 var parseCookiePhantomjs = require('parse-cookie-phantomjs');
 var phantomBridge = require('phantom-bridge');
 var objectAssign = require('object-assign');
 var es5shim;
+
+function handleCookies(cookies, url) {
+	var parsedUrl = urlMod.parse(url);
+
+	return (cookies || []).map(function (cookie) {
+		var ret = typeof cookie === 'string' ? parseCookiePhantomjs(cookie) : cookie;
+
+		if (!ret.domain) {
+			ret.domain = parsedUrl.hostname;
+		}
+
+		if (!ret.path) {
+			ret.path = parsedUrl.path;
+		}
+
+		return ret;
+	});
+}
 
 module.exports = function (url, size, opts) {
 	opts = objectAssign({
@@ -19,9 +38,7 @@ module.exports = function (url, size, opts) {
 	opts.height = size.split(/x/i)[1] * opts.scale;
 	opts.es5shim = opts.es5shim !== false ? path.relative(path.join(__dirname, 'lib'), es5Shim) : null;
 	opts.format = opts.format === 'jpg' ? 'jpeg' : opts.format ? opts.format : 'png';
-	opts.cookies = (opts.cookies || []).map(function (cookie) {
-		return typeof cookie === 'string' ? parseCookiePhantomjs(cookie) : cookie;
-	});
+	opts.cookies = handleCookies(opts.cookies, opts.url);
 
 	if (opts.es5shim) {
 		es5shim = fs.readFileSync(es5Shim, 'utf8');
